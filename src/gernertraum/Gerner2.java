6490f35ffb28472f32041df9db138d43210c6c63
VeitSchilcher <VeitSchilcher@DESKTOP-KKG2CO5>
2020-05-05 11:15:02 +0200

    Great commit

---
+++ src/gernertraum/Gerner2.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gernertraum;
+
+/**
+ *
+ * @author VeitSchilcher
+ */
+public class Gerner2 {
+    public void m(){
+        System.out.println("In m");
+    }         
+}
